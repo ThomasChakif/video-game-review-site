3303e61328d755b020c0fa4a85219b4055fda134
thc225 <thc225@lehigh.edu>
2025-06-12 10:13:15 -0400

    updates appheader design

--- frontend/src/components/AppHeader.jsx
+++ frontend/src/components/AppHeader.jsx
@@ -2,7 +2,6 @@ import * as React from 'react';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import '../styles/AppHeader.css'
 import { useNavigate } from 'react-router-dom'
@@ -11,16 +10,16 @@ function AppHeader() {
 
     const navigate = useNavigate()
     return (
-        <Box sx={{ flexGrow: 1 }}>
-        <AppBar sx={{background: 'black'}}position="static">
-            <Toolbar>
-            <Typography onClick={() => navigate('/')} className='VGR-header' variant="h4" component="div" sx={{ flexGrow: 1}}>
-                VGR
-            </Typography>
-            <Button onClick={() => navigate('/user')} color="inherit">Account</Button>
-            <Button onClick={() => navigate('/logout')} color="inherit">Logout</Button>
-            </Toolbar>
-        </AppBar>
+        <Box>
+            <AppBar sx={{background: '#111827', borderRadius: "0 0 25px 25px;"}}position="static">
+                <Toolbar>
+                    <h1 onClick={() => navigate('/')} className='VGR-header'>
+                        VGR
+                    </h1>
+                    <Button onClick={() => navigate('/user')} color="inherit" sx={{margin: '1rem'}}>Account</Button>
+                    <Button onClick={() => navigate('/logout')} color="inherit" sx={{margin: '1rem'}}>Logout</Button>
+                </Toolbar>
+            </AppBar>
         </Box>
     );
 }
