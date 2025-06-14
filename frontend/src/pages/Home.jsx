6dcfdce7b59f310f9c47a628c4b3a73615314e23
thc225 <thc225@lehigh.edu>
2025-06-14 19:34:10 -0400

    added game sorting to home page

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -9,14 +9,26 @@ function Home() {
     const [gameData, setGameData] = useState([])
     const navigate = useNavigate()
 
+    // retrieve games and sort them alphabetically by their game names
     useEffect(() => {
         fetch('../gameData.json')
         .then(res => res.json())
-        .then(data => setGameData(data.games))
+        .then(data => {
+            const sortedGames = data.games.sort((a, b) => {
+                const firstName = a.game_name.toLowerCase().trim();
+                const secondName = b.game_name.toLowerCase().trim();
+
+                if(firstName < secondName) return -1;
+                if(firstName > secondName) return 1;
+                return 0;
+            });
+            setGameData(sortedGames)
+        })
     })
 
     return <div className='page'>
         <AppHeader />
+        <h1 className='home-header'>Click on any of the games below to be taken to their page!</h1>
         <Grid container direction = "row" spacing={1}
             sx={{
                 justifyContent: "center",
