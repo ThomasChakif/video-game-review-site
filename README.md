0d134f872b57f14b2a72a195b0a3a03d8ebea167
thc225 <thc225@lehigh.edu>
2025-07-22 10:56:53 -0400

    added game ratings to home page

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 ### Project Description
 * This website utilizes a React-based frontend with a Python/Django backend. Users are able to sign in, view a list of games, and visit their pages to see a description of the game, reviews that were left, as well as leave a review on their own. Users can see/delete all of their own reviews from the 'Account' tab.
 ![Home page](https://github.com/ThomasChakif/video-game-review-site/blob/main/frontend/src/img/vgr-homepage.png)
-![Game page](https://github.com/ThomasChakif/video-game-review-site/blob/main/frontend/src/img/vgr-home-games.png)
+![Game page](https://github.com/ThomasChakif/video-game-review-site/blob/main/frontend/src/img/vgr-home-games-new.png)
 ### Project Specifications
 * User Accounts & Roles: Users are able to sign in and are authenticates using JWT Access and Refresh tokens. Once signed in, users are presented with a list of available games on the site. Upon visiting a game page, users can see a short description of the game, the game's cover and publisher, as well as an aggregated score of all reviews left. Users can also see a list of all made reviews, and can leave a review if they have not already left a review for that same game. Users are able to visit their 'Account' page and see and delete any past reviews they've made. Users can visit the 'Help' page to submit a help request/email through FormSubmit.
 
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -3,11 +3,61 @@ import Grid from "@mui/material/Grid";
 import { useNavigate } from "react-router-dom";
 import '../styles/Home.css'
 import AppHeader from "../components/AppHeader";
+import api from "../api"
+import Box from '@mui/material/Box';
 
 function Home() {
 
     const [gameData, setGameData] = useState([])
+    const [averages, setAverages] = useState({})
     const navigate = useNavigate()
+
+    const getRatingColor = (average) => {
+            const value = parseFloat(average)
+                switch (true) {
+                    case value <= 1.0:
+                        return '#FD0101';
+                    case value <= 2.0:
+                        return '#FC3201';
+                    case value <= 3.0:
+                        return '#FC9901';
+                    case value <= 4.0:
+                        return '#CC9901';
+                    case value <= 5.0:
+                        return '#989901';
+                    case value <= 6.0:
+                        return '#669901';
+                    case value <= 7.0:
+                        return '#339901';
+                    case value <= 8.0:
+                        return '#119901';    
+                    case value <= 9.0:
+                        return '#017701';
+                    case value > 9.0:
+                        return '#005400'
+                    default:
+                        return 'black';
+            }
+    }
+    
+    //fetch the average rating for each game and set it linked to the game_ID it's linked to so we can print on the home screen
+    const getAverage = async (game_ID) => {
+        try{
+            const res = await api.get(`/api/reviews/average-rating/${game_ID}/`)
+            const data = res.data;
+            setAverages(prev => ({
+                ...prev,
+                [game_ID]: data.avg ?? 7.00 //link average to the game id
+            }));
+        }catch (err){
+            console.error(`Error fetching average for game ${game_ID}:`, err);
+            //set default rating to 7 in case of error
+            setAverages(prev => ({
+                ...prev,
+                [game_ID]: 7.00
+            }));
+        }
+    }
 
     // retrieve games and sort them alphabetically by their game names
     useEffect(() => {
@@ -26,6 +76,16 @@ function Home() {
         })
     })
 
+    //get the average for each game to display alongside title
+    useEffect(() => {
+        if(gameData.length > 0){
+            gameData.forEach(game => {
+                getAverage(game.game_id)
+            });
+        }
+    }, [gameData])
+
+
     return <div className='page'>
         <AppHeader />
         <h1 className='home-header'>Click on any of the games below to be taken to their page!</h1>
@@ -36,29 +96,51 @@ function Home() {
             }}>
             {gameData.map((game) => (
                 <div>
-                <p className = 'game-titles'>{game.game_name}
-                </p>
-                <Grid className = 'game-card' item key={game.game_id} 
-                sx={{ 
-                    backgroundImage:`url(${game.game_cover})`,
-                    backgroundRepeat: "no-repeat",
-                    backgroundSize: "cover",
-                    backgroundPosition: "center",
-                    height: "500px",
-                    postition: "relative",
-                    margin: "1rem 2rem 2rem 2rem", //need margin for proper row spacing
-                    borderRadius:'10px',
-                    borderColor: 'white',
-                    borderStyle: 'solid',
-                    width: "300px"
-                }}>
-                <button 
-                    className = 'game-button'
-                    onClick={() => navigate(`/game/${game.game_id}`)}>
-                        VISIT PAGE
-                    </button>
-                </Grid>
-                 </div>
+                    <p className = 'game-titles'>{game.game_name}</p>
+                    <Grid className = 'game-card' item key={game.game_id} 
+                    sx={{ 
+                        backgroundImage:`url(${game.game_cover})`,
+                        backgroundRepeat: "no-repeat",
+                        backgroundSize: "cover",
+                        backgroundPosition: "center",
+                        height: "500px",
+                        position: "relative",
+                        margin: "1rem 2rem 2rem 2rem", //need margin for proper row spacing
+                        borderRadius:'10px',
+                        borderColor: 'white',
+                        borderStyle: 'solid',
+                        width: "300px"
+                    }}>
+                    {/* rating circle in top right */}
+                    <Box
+                        className='game-rating'
+                        sx={{
+                        position: "absolute",
+                        top: "10px",
+                        right: "10px",
+                        width: "60px",
+                        height: "60px",
+                        backgroundColor: getRatingColor(averages[game.game_id]), //change color based on rating number
+                        borderRadius: "50%",
+                        display: "flex",
+                        alignItems: "center",
+                        justifyContent: "center",
+                        color: "white",
+                        fontWeight: "bold",
+                        fontSize: "2rem",
+                        zIndex: 1 // Ensure it appears above background
+                        }}
+                    >
+                        {/* display  average rating in circle*/}
+                        {averages[game.game_id] ? averages[game.game_id].toFixed(1) : "?"}
+                    </Box>
+                    <button 
+                        className = 'game-button'
+                        onClick={() => navigate(`/game/${game.game_id}`)}>
+                            VISIT PAGE
+                        </button>
+                    </Grid>
+                </div>
             ))}
         </Grid>
     </div>
